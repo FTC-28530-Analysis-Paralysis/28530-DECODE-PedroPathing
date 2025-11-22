7919ac5a70412b3c09d3f8a7589e6bb21ff667ff
Mahlon <mahlon.philip.manson@gmail.com>
2025-11-21 17:04:19 -0700

    Work on

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/KalispellAuto.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//import org.firstinspires.ftc.teamcode.RobotHardware.IntakeHardware;
+//import org.firstinspires.ftc.teamcode.RobotHardware.LauncherHardware;
+//import org.firstinspires.ftc.teamcode.RobotHardware.MecanumHardware;
+//import org.firstinspires.ftc.teamcode.RobotHardware.TransferHardware;
+
+
+@Autonomous(name="KalispellAuto", group="01 Kalispell")
+public class KalispellAuto extends OpMode{
+    HardwareMap hwMap = new HardwareMap();
+//    IntakeHardware intakeHardware = new IntakeHardware();
+//    MecanumHardware mecanumHardware = new MecanumHardware();
+//    TransferHardware transferHardware = new TransferHardware();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init(){
+        hwMap.init(hardwareMap);
+//        intakeHardware.init(hardwareMap);
+//        mecanumHardware.init(hardwareMap);
+//        transferHardware.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+
+        // Drive forward for 2 seconds
+        while (runtime.seconds() < 2) {
+
+            hwMap.drive(.2, 0, 0);
+            telemetry.addData("Status", "Autonomous Running");
+            telemetry.update();
+
+            if(runtime.seconds()> 2){
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Status", "Autonomous Finished");
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+        hwMap.stop();
+//        transferHardware.stop();
+//        intakeHardware.stop();
+//        launcherHardware.stop();
+
+    }
+
+}
